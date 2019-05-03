a89d4e3c863a44d66651f8d4d305d3f834591288
Chirag Rao <chiragr6192@algonquinlive.com>
2019-05-03 16:27:56 -0400

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,3 @@
-console.log('file loaded')
-
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.getElementById('message-1')
